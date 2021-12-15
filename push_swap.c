901a0b02c732b585dd58bcacc3543f38a246c334
Nathan BENHADOUCHE <nbenhado@e3r7p3.42.fr>
2021-12-15 15:44:08 +0100

    sur les fonctions de push

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: nbenhado <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 14:36:19 by nbenhado          #+#    #+#             */
-/*   Updated: 2021/12/14 21:34:22 by nbenhado         ###   ########.fr       */
+/*   Updated: 2021/12/15 15:43:26 by nbenhado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,6 +44,7 @@
 	printf("--------\n");
 }*/
 
+
 void	print_stack(char **a, char **b, int number)
 {
 	int i;
@@ -70,7 +71,7 @@ char **init_stack_b(int numb)
 	tab_de_str = malloc(sizeof(char **) * numb);
 	while (i + 1 < numb)
 	{
-		tab_de_str[i] = "(vide)";
+		tab_de_str[i] = "";
 		i++;
 	}
 	tab_de_str[i] = 0;
@@ -93,15 +94,29 @@ char **init_stack_a(char **argv, int numb)
 	tab_de_str[i] = 0;
 	return (tab_de_str);
 }
+//renvoi l'emplacement de la premiere case vide trouver (ou le premier si full)
+int empty_or_max(char **tab, int max)
+{
+	int i;
+
+	i = 0;
+	if (!tab)
+		return (0);
+	while (max  > 0)
+	{
+		max--;	
+		if (!tab[max])
+			return (max - 1);
+	}
+	return (1);
+}
 
 void	swap_b(char **tab)
 {
-	char	i;
-	if (!tab)
-		printf("vide");
-	i = *tab[0];
+	char *tmp;
+	tmp = tab[0];
 	tab[0] = tab[1];
-	*tab[1] = i;
+	tab[1] = tmp;
 }
 
 void	swap_a(char **tab)
@@ -112,15 +127,11 @@ void	swap_a(char **tab)
 	tab[1] = tmp;
 }
 
-void	swap_ss(char **a, char **b, int max)
+// prend le premier element de a (le plus haut) et le met dans b (le plus bas possible), ne fait rient sur a est vide
+void	push_b(char **a, char **b, int max)
 {
-	b[max - 2] = a[0];
-	a[0] = "(vide)";
-}
-
-void	push_b(int *a, int *b)
-{
-	b[SIZE - 2] = a[0];		
+	b[empty_or_max(b, max)] = a[empty_or_max(a, max)];
+	a[empty_or_max(a, max)] = "";
 }
 
 int main(int ac, char **av)
@@ -128,13 +139,14 @@ int main(int ac, char **av)
 	char **a;
 	char **b;
 
+	printf("valeur de AC : %d\n", ac);
 	a = init_stack_a(av, ac);
 	b = init_stack_b(ac);
 	//printf("%s\n", tab1[1]);
 	print_stack(a, b, ac);
-	swap_ss(a, b, ac);
+	push_b(a, b, ac);
 	print_stack(a, b, ac);
-
+	printf("%d\n", empty_or_max(a, ac));
 
 	return (0);
 }
