97cdd37b3855e80c04f37a24b2cc4a4aef9df3be
Nathan BENHADOUCHE <nbenhado@e3r8p19.clusters.42paris.fr>
2021-12-19 18:28:03 +0100

    decouverte et creation dun autre type dalgo, plus long pour linstant

--- main.c
+++ main.c
@@ -81,41 +81,7 @@ void	three_numbers(char **a, int down)
 
 // }
 
-// void	main_algo(char **a, char **b, int down)
-// {
-// 	int i;
-	
-// 	if(swap_checker(a, down) == 0)
-// 	{
-// 		ft_putstr_fd("Error\n", 2);
-// 		return ;
-// 	}
-// 	i = 0;
-// 	while (1)
-// 	{
-		
-// 		if (ft_atoi(a[before_empty_case(a, down)]) == minimal_value(a, down))
-// 		{
-// 			push_b(a, b, down);
-// 			i++;
-// 			if (isempty(a, down))
-// 				break ;
-// 		}
-// 		if (!near_down(a, minimal_value(a, down), down))
-// 		 	rotate_a(a, down);
-// 		 else
-// 			reverse_rotate_a(a, down);
-// 		i++;
-// 	}
-// 	while (!isempty(b, down))
-// 	{
-// 		push_a(a, b, down);
-// 		i++;
-// 	}
-// 	//printf("\nnombre doperations : %d\n\n", i);
-// }
-
-void	big_stack_sorting(char **a, char **b, int down)
+void	main_algo(char **a, char **b, int down)
 {
 	int i;
 	
@@ -149,13 +115,35 @@ void	big_stack_sorting(char **a, char **b, int down)
 	//printf("\nnombre doperations : %d\n\n", i);
 }
 
+void	big_stack_sorting(char **a, char **b, int down)
+{
+	if(swap_checker(a, down) == 0)
+	{
+		ft_putstr_fd("Error\n", 2);
+		return ;
+	}
+	while (!isempty(a, down))
+	{
+		while (!isempty(b, down) && ft_atoi(b[before_empty_case(b, down)]) > ft_atoi(a[before_empty_case(a, down)]))
+		{
+			push_a(a, b, down);
+			swap_a(a, down);
+		}
+		push_b(a, b, down);
+	}
+	while (!isempty(b, down))
+		push_a(a, b, down);
+
+	//printf("\nnombre doperations : %d\n\n", i);
+}
+
 
  int main(int ac, char **av)
  {
 	char **a;
 	char **b;
 	 
-	// printf("\nvaleur de AC : %d\n", ac);
+	//printf("\nvaleur de AC : %d\n", ac);
 	a = init_stack_a(av, &ac);
 	b = init_stack_b(ac);
 	// printf("valeur de AC apres init : %d\n", ac);
@@ -167,7 +155,10 @@ void	big_stack_sorting(char **a, char **b, int down)
 	// else if (ac == 5 || ac == 6)
 
 	else
+	{
+		// big_stack_sorting(a, b, ac);
 		main_algo(a, b, ac);
+	}
 
  	return (0);
  }
